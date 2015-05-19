103dfa2dbef17c3b2abe3841802e749a1db90a74
Emerique <bigzine@gmail.com>
2015-05-19 15:39:33 +0200

    edit hgigore

---
+++ TTAPIMVVMU/Model/ServerTTapi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using TTAPI_Init_Worker_XT;
+
+
+namespace TTAPIMVVMU.Model
+{
+    public class ServerTTapi :IConnection
+    {
+        public void Connect()
+        {
+            {
+                // Dictates whether TT API will be started on its own thread
+                bool startOnSeparateThread = true;
+
+                if (startOnSeparateThread)
+                {
+                    // Start TT API on a separate thread
+                    TTAPIFunctions tf = new TTAPIFunctions();
+                    Thread workerThread = new Thread(tf.Start);
+                    workerThread.Name = "TT API Thread";
+                    workerThread.Start();
+
+                    // Insert other code here that will run on this thread
+                }
+                else
+                {
+                    // Start the TT API on the same thread
+                    using (TTAPIFunctions tf = new TTAPIFunctions())
+                    {
+                        tf.Start();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TTAPIMVVMU/Model/TTAPIFunctions.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TTAPI_Init_Worker_XT
+{
+    using TradingTechnologies.TTAPI;
+
+    /// <summary>
+    /// Main TT API class
+    /// </summary>
+    class TTAPIFunctions : IDisposable
+    {
+        /// <summary>
+        /// Declare the TTAPI objects
+        /// </summary>
+        private XTraderModeTTAPI m_apiInstance = null;
+        private WorkerDispatcher m_disp = null;
+        private bool m_disposed = false;
+        private object m_lock = new object();
+
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TTAPIFunctions()
+        {
+        }
+
+        /// <summary>
+        /// Create and start the Dispatcher
+        /// </summary>
+        public void Start()
+        {
+            // Attach a WorkerDispatcher to the current thread
+            m_disp = Dispatcher.AttachWorkerDispatcher();
+            m_disp.BeginInvoke(Init);
+            m_disp.Run();
+        }
+
+        /// <summary>
+        /// Initialize TT API
+        /// </summary>
+        private void Init()
+        {
+            // Use "Follow X_TRADER" Login Mode
+            ApiInitializeHandler h = ttApiInitComplete;
+            TTAPI.CreateXTraderModeTTAPI(Dispatcher.Current, h);
+        }
+
+        /// <summary>
+        /// Event notification for status of TT API initialization
+        /// </summary>
+        private void ttApiInitComplete(TTAPI api, ApiCreationException ex)
+        {
+            if (ex == null)
+            {
+                // Authenticate your credentials
+                m_apiInstance = (XTraderModeTTAPI)api;
+                m_apiInstance.ConnectionStatusUpdate += new EventHandler<ConnectionStatusUpdateEventArgs>(m_apiInstance_ConnectionStatusUpdate);
+                m_apiInstance.Start();
+            }
+            else
+            {
+                Dispose();
+                throw new Exception("TT API Initialization Failed: {0}"+ ex.Message);
+         
+            }
+
+        }
+
+        /// <summary>
+        /// Event notification for status of authentication
+        /// </summary>
+        public void m_apiInstance_ConnectionStatusUpdate(object sender, ConnectionStatusUpdateEventArgs e)
+        {
+            if (e.Status.IsSuccess)
+            {
+                // Add code here to begin working with the TT API
+            }
+            else
+            {
+               Dispose();
+                throw new Exception("TT API Initialization Failed: {0}"+ e.Status.StatusMessage);
+            }
+        }
+
+        /// <summary>
+        /// Shuts down the TT API
+        /// </summary>
+        public void Dispose()
+        {
+            lock (m_lock)
+            {
+                if (!m_disposed)
+                {
+                    // Begin shutdown the TT API
+                    TTAPI.ShutdownCompleted += new EventHandler(TTAPI_ShutdownCompleted);
+                    TTAPI.Shutdown();
+
+                    m_disposed = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Event notification for completion of TT API shutdown
+        /// </summary>
+        public void TTAPI_ShutdownCompleted(object sender, EventArgs e)
+        {
+            // Shutdown the Dispatcher
+            if (m_disp != null)
+            {
+                m_disp.BeginInvokeShutdown();
+                m_disp = null;
+            }
+
+            // Dispose of any other objects / resources
+        }
+    }
+}
